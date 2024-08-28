9d98b52bec77d1f57c026a734ad4cc6289068dc6
Juan-Bagot <165488742+Juan-Bagot@users.noreply.github.com>
2024-08-28 20:16:06 -0300

    Update en Controlador Usuario

--- src/logica/ControladorUsuario.java
+++ src/logica/ControladorUsuario.java
@@ -3,20 +3,29 @@ package logica;
 import logica.Usuario;
 import logica.Deportista;
 import logica.Entrenador;
+import java.time.LocalDate;
+import excepciones.*;
 
-import jakarta.persistence.*;
+//import jakarta.persistence.*;
 
 
 public class ControladorUsuario {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("airelibre.uy");
-    private EntityManager em = emf.createEntityManager();
-    private EntityTransaction tx = em.getTransaction();
+    //private EntityManagerFactory emf = Persistence.createEntityManagerFactory("airelibre.uy");
+    //private EntityManager em = emf.createEntityManager();
+    //private EntityTransaction tx = em.getTransaction();
     //tx.begin();
     //em.persist(usu);
     //tx.commit();
     //em.close();
     //emf.close();
+    public ControladorUsuario() {}
+    
+    public void altaUsuario(String nickname, String nombre, String apellido, String email, LocalDate fechaNacimiento, String contrasena) {
+    	
+    }
+    
+    public void consultaUsuario() {
+    	
+    }
 }
-
-HOLAAAasd maurogai
