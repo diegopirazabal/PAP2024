19602690e2ff6372fdad5ca4936f54207d6670c6
Diego Pereyra <diegopirazabal@gmail.com>
2024-09-01 12:09:39 -0300

    pr

--- src/logica/Inscripcion.java
+++ src/logica/Inscripcion.java
@@ -1,14 +1,9 @@
 package logica;
-<<<<<<< HEAD
-import java.time.LocalDate;
-import javax.persistence.*;
-=======
 import logica.Usuario;
 import logica.Usuario.Deportista;
 import logica.Usuario.Entrenador;
 import java.time.LocalDate;
 import jakarta.persistence.*;
->>>>>>> cantomauro
 
 @Entity
 @Table(name = "INSCRIPCIONES")
@@ -16,15 +11,9 @@ public class Inscripcion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
-    private int id;
-
-    @Column(name = "Fecha", nullable = false)
-=======
     private Long id;
 
     @Column(name = "FechaInscripcion", nullable = false)
->>>>>>> cantomauro
     private LocalDate fechaInscripcion;
 
     @Column(name = "CantidadDeportistas", nullable = false)
@@ -32,28 +21,6 @@ public class Inscripcion {
 
     @Column(name = "Costo", nullable = false)
     private double costo;
-<<<<<<< HEAD
-
-    @ManyToOne
-    private Clase clase; // Relación con la clase
-
-    @ManyToOne
-    private Deportista deportista; // Relación con el deportista
-
-    // Constructor sin parámetros
-    public Inscripcion() {}
-
-    // Constructor con parámetros
-    public Inscripcion(LocalDate fechaInscripcion, int cantidadDeportistas, double costo, Clase clase, Deportista deportista) {
-        this.fechaInscripcion = fechaInscripcion;
-        this.cantidadDeportistas = cantidadDeportistas;
-        this.costo = costo;
-        this.clase = clase;
-        this.deportista = deportista;
-    }
-
-    // Getters y Setters
-=======
 
     @ManyToOne
     @JoinColumn(name = "clase_id")
@@ -63,7 +30,6 @@ public class Inscripcion {
     @JoinColumn(name = "deportista_id")
     private Deportista deportista;
 
->>>>>>> cantomauro
     public LocalDate getFechaInscripcion() {
         return fechaInscripcion;
     }
