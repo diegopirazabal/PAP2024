f11d995c00b4792af837834bb57565604f27485b
diegopirazabal <diegopirazabal@gmail.com>
2024-09-02 23:23:08 -0300

    aaayuda

--- src/logica/controladorUsuario.java
+++ src/logica/controladorUsuario.java
@@ -1,57 +1,63 @@
 package logica;
 import logica.Usuario;
-import persistencia.Persistencia;
-
-import java.sql.Date;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
+
 import excepciones.*;
-import dtos.*;
 
-public class controladorUsuario {
-    private manejadorUsuarios manejador;
+public class ControladorUsuario  implements IControladorUsuario{
+    private manejadorUsuario manejador;
 
-    public controladorUsuario() {
-        manejador = new manejadorUsuarios();
+    public ControladorUsuario() {
+        manejador = new manejadorUsuario();
     }
 
-    public void crearDeportista(String nickname, String nombre, String apellido, String email, String fechaNacimiento, String contrasena, boolean esProfesional) throws UsuarioRepetidoException {
-        Deportista deportista = new Deportista (nickname, nombre, apellido, email, fechaNacimiento, contrasena, esProfesional);
+    public void crearDeportista(String nickname, String nombre, String apellido, String email, LocalDate fechaNacimiento, String contrasena, boolean esProfesional) throws UsuarioRepetidoException {
+        Usuario deportista = new Deportista(nickname, nombre, apellido, email, fechaNacimiento, contrasena, esProfesional);
        
         manejador.agregar(deportista);
     }
 
-    public void crearEntrenador(String nickname, String nombre, String apellido, String email, String fechaNacimiento, String contrasena, String disciplina, String linkSitioWeb) throws UsuarioRepetidoException {
-    	Entrenador entrenador = new Entrenador(nickname, nombre, apellido, email, fechaNacimiento, contrasena, disciplina, linkSitioWeb);
+    public void crearEntrenador(String nickname, String nombre, String apellido, String email, LocalDate fechaNacimiento, String contrasena, String disciplina, String linkSitioWeb) throws UsuarioRepetidoException {
+    	Usuario entrenador = new Entrenador(nickname, nombre, apellido, email, fechaNacimiento, contrasena, disciplina, linkSitioWeb);
     	manejador.agregar(entrenador);    	
     }
     
-    	public Usuario consultarUsuario(String nickname) throws UsuarioNoExisteException {
-        return manejador.buscarUsuario(nickname);
+    public Usuario consultarUsuario(String nickname) throws UsuarioNoExisteException {
+    	return manejador.buscarUsuario(nickname);
     }
 
     public void eliminarUsuario(String nickname) throws UsuarioNoExisteException{
     	 manejador.eliminar(nickname);
     }
+    
+    public List<Usuario> listarTodos(){
+    	return manejador.obtenerTodos();
+    }
+    
+    public DataUsuario[] getUsuarios() throws UsuarioNoExisteException {
+        manejadorUsuario mu = manejador.getinstance();
+        Usuario[] usrs = mu.getUsuarios();  // Usa el getUsuarios que devuelve array de objetos
 
-//    public List<dataTypeUsuario> obtenerTodos() {
-//        try {
-//            List<Usuario> usuarios = em.createQuery("SELECT u FROM Usuario u", Usuario.class).getResultList();
-//            return usuarios.stream()
-//                    .map(usuario -> new dataTypeUsuario(
-//                            usuario.getNickname(),
-//                            usuario.getNombre(),
-//                            usuario.getApellido(),
-//                            usuario.getEmail(),
-//                            usuario.getFechaNacimiento()))
-//                    .collect(Collectors.toList());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return new ArrayList<>();
-//        }
-//    }
+        if (usrs != null) {
+            DataUsuario[] du = new DataUsuario[usrs.length];
+            Usuario usuario;
+
+            // Para separar logica de presentacion, no se deben devolver los Usuario,
+            // sino los DataUsuario
+            for (int i = 0; i < usrs.length; i++) {
+                usuario = usrs[i];
+                du[i] = new DataUsuario(usuario.getNombre(), usuario.getApellido(), usuario.getCedulaIdentidad());
+                // pasa el array de objetos a array de DataUsuaios
+            }
+
+            return du;
+        } else
+            throw new UsuarioNoExisteException("No existen usuarios registrados");
+    }
 }
+
+	
 /*
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("airelibre.uy");
     private EntityManager em = emf.createEntityManager();
